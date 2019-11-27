da0f1e3eb078e85dc65519a7acb6318eb975517b
1173710230 <1250366059@qq>
2019-11-27 14:31:38 +0800

    Test Commit

--- src/main/java/com/example/project/domain/Customer.java
+++ src/main/java/com/example/project/domain/Customer.java
@@ -1,4 +1,4 @@
-package com.example.demo.domain;
+package com.example.project.domain;
 
 import java.util.List;
 import java.util.Objects;
--- src/main/java/com/example/project/domain/InputOrder.java
+++ src/main/java/com/example/project/domain/InputOrder.java
@@ -1,4 +1,4 @@
-package com.example.demo.domain;
+package com.example.project.domain;
 
 import java.util.Date;
 import java.util.Objects;
