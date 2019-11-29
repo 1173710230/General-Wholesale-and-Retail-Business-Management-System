26f2f2819c1dcf150b4e78437a5762e3a1de01ae
1173710230 <1250366059@qq>
2019-11-29 23:38:04 +0800

    Service 2.3

--- src/main/java/com/example/project/server/SellOrderService.java
+++ src/main/java/com/example/project/server/SellOrderService.java
@@ -9,20 +9,60 @@ import java.util.List;
 @Service
 public interface SellOrderService {
 
+
+    /**
+     * 添加一个销售单
+     *
+     */
     boolean addSellOrder(Date date, int goodsId, double sellUnitPrice, int sellSum, int customerId);
 
+
+    /**
+     * 更新销售单，变化内容存到参数中
+     *
+     */
     boolean modifySellOrder(SellOrder sellOrder);
 
+
+    /**
+     * 按id删除销售单
+     *
+     */
     boolean deleteSellOrder(int sellOrderId);
 
+
+    /**
+     * 获取未确认的销售单
+     *
+     */
     List<SellOrder> getUncheckedSellOrder();
 
+
+    /**
+     * 审核销售单
+     *
+     */
     boolean checkOrder(int sellOrderId);
 
+
+    /**
+     * 付款
+     *
+     */
     boolean paySellOrder(int sellOrderId);
 
+
+    /**
+     * 按id退款
+     *
+     */
     boolean refundSellOrder(int sellOrderId);
 
+
+    /**
+     * 获取未付款的订单
+     *
+     */
     List<SellOrder> getUnpaidOrder();
 
 }
