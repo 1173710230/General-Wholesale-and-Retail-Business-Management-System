f5c08762b29e34c938c02a957ac4f0b17fc78379
1173710230 <1250366059@qq>
2019-11-30 00:08:28 +0800

    Service 2.4

--- src/main/java/com/example/project/server/GoodsServiceImpl.java
+++ src/main/java/com/example/project/server/GoodsServiceImpl.java
@@ -1,29 +1,70 @@
 package com.example.project.server;
 
+import com.example.project.dao.*;
 import com.example.project.domain.Goods;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class GoodsServiceImpl implements GoodsService{
+
+    private final GoodsMapper goodsMapper;
+
+    private final CustomerMapper customerMapper;
+
+    private final SellOrderMapper sellOrderMapper;
+
+    private final UserMapper userMapper;
+
+    private final WareHouseMapper wareHouseMapper;
+
+    @Autowired
+    public GoodsServiceImpl(GoodsMapper goodsMapper,
+                            CustomerMapper customerMapper,
+                            SellOrderMapper sellOrderMapper,
+                            UserMapper userMapper,
+                            WareHouseMapper wareHouseMapper) {
+        this.goodsMapper = goodsMapper;
+        this.customerMapper = customerMapper;
+        this.sellOrderMapper = sellOrderMapper;
+        this.userMapper = userMapper;
+        this.wareHouseMapper = wareHouseMapper;
+    }
+
     @Override
     public List<Goods> getallWirehouseGoods() {
+
         return null;
     }
 
     @Override
     public boolean deleteGoodsById(int Goods_id) {
+
         return false;
     }
 
     @Override
-    public boolean modifyGoodsById(int Goods_id) {
-        return false;
+    public boolean modifyGoodsById(int goodsId, String goodsName, String spec) {
+        Goods goods = new Goods();
+        goods.setGoodsName(goodsName);
+        goods.setGoodsSpecification(spec);
+        goods.setGoodsId(goodsId);
+        try {
+            goodsMapper.updateGoods(goods);
+            return true;
+        } catch (DataAccessException ex) {
+            return false;
+        }
     }
 
     @Override
     public List<Goods> queryGoodsByName(String name) {
+        Goods goods = new Goods();
+        goods.setGoodsName(name);
+        goodsMapper.queryGoods(goods);
         return null;
     }
 }
--- src/main/java/com/example/project/server/WarehouseServiceImpl.java
+++ src/main/java/com/example/project/server/WarehouseServiceImpl.java
@@ -1,24 +1,55 @@
 package com.example.project.server;
 
+import com.example.project.dao.*;
 import com.example.project.domain.Goods;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class WarehouseServiceImpl implements WarehouseService{
+
+    private final GoodsMapper goodsMapper;
+
+    private final CustomerMapper customerMapper;
+
+    private final SellOrderMapper sellOrderMapper;
+
+    private final UserMapper userMapper;
+
+    private final WareHouseMapper wareHouseMapper;
+
+    @Autowired
+    public WarehouseServiceImpl(GoodsMapper goodsMapper,
+                                CustomerMapper customerMapper,
+                                SellOrderMapper sellOrderMapper,
+                                UserMapper userMapper,
+                                WareHouseMapper wareHouseMapper) {
+        this.goodsMapper = goodsMapper;
+        this.customerMapper = customerMapper;
+        this.sellOrderMapper = sellOrderMapper;
+        this.userMapper = userMapper;
+        this.wareHouseMapper = wareHouseMapper;
+    }
+
     @Override
     public List<Goods> getGoodsfromallWarehouse() {
-        return null;
+        Goods goods = new Goods();
+        return goodsMapper.queryGoods(goods);
     }
 
     @Override
     public List<Goods> queryGoodsByName(String name) {
-        return null;
+        Goods goods = new Goods();
+        goods.setGoodsName(name);
+        return goodsMapper.queryGoods(goods);
     }
 
     @Override
     public boolean addNewGoods(int checkgoodsId, int goodsnumber, String goodsName, String spec, int warehouseId) {
+
+
         return false;
     }
 }
