0d2528fee0506811974ea4410edade12158d72fe
zhuyeqi <947996900@qq.com>
2019-12-13 17:26:55 +0800

    调仓数据库实现

--- src/main/java/com/example/project/dao/WareHouseMapper.java
+++ src/main/java/com/example/project/dao/WareHouseMapper.java
@@ -4,6 +4,7 @@ package com.example.project.dao;
 import com.example.project.domain.Goods;
 import com.example.project.domain.Warehouse;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -32,5 +33,19 @@ public interface WareHouseMapper {
      * @param newName 新的名字
      */
     void updateWarehouseNameById(int id, String newName);
+
+    /**
+     * 调仓。
+     * 转出仓库ID，转入仓库ID，货物ID有任意一个不存在，都会报错。
+     *
+     * @param goodsId        货物ID
+     * @param oldWarehouseId 转出仓库ID
+     * @param newWarehouseId 转入仓库ID
+     * @param number         数量
+     */
+    void move(@Param("goodsId") int goodsId,
+              @Param("oldWarehouseId") int oldWarehouseId,
+              @Param("newWarehouseId") int newWarehouseId,
+              @Param("number") double number);
 
 }
