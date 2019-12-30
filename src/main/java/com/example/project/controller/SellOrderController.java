52c914949e275f690fc51418e47484b263680132
Chengfei Liu <18837059208@163.com>
2019-12-30 17:11:52 +0800

    获取积分兑换比例方法

--- src/main/java/com/example/project/controller/SellOrderController.java
+++ src/main/java/com/example/project/controller/SellOrderController.java
@@ -1,22 +1,17 @@
 package com.example.project.controller;
 
-import com.example.project.domain.SellOrder ;
+import com.example.project.domain.Host;
+import com.example.project.domain.SellOrder;
 import com.example.project.domain.SellOrderGroup;
 import com.example.project.service.SellOrderService;
 import com.example.project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -252,4 +247,13 @@ public class SellOrderController{
     return sellOrderService.changeIntegralRatio(integralRatio);
   }
 
+  /**
+   * 获取积分兑换比例
+   * @return 积分兑换比率
+   */
+  @RequestMapping("/getIntegralRatio")
+  @ResponseBody
+  public double getIntegralRatio(){
+    return Host.getIntegralRatio();
+  }
 }
